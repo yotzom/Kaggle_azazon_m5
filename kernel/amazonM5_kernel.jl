af2ac27c3a1320188f382d23946d6b4c8e00510f
Cc.yotzom <yotsom1009@gmail.com>
2020-03-31 23:54:28 +0800

    loadMultiData() & searchArray() v1 finished, and add how to use

--- kernel/amazonM5_kernel.jl
+++ kernel/amazonM5_kernel.jl
@@ -1,30 +1,11 @@
 #homedir()
 #pwd()
 #Set work directory
-#cd("C:\\Users\\CCYoTzom\\Desktop\\kaggle_azazon_m5\\kernel")
-cd("D:\\Jace\\Project\\98.Learning\\Kaggle_azazon_m5\\kernel")
+cd("C:\\Users\\CCYoTzom\\Desktop\\kaggle_azazon_m5\\kernel")
+#cd("D:\\Jace\\Project\\98.Learning\\Kaggle_azazon_m5\\kernel")
 
 using CSV
 using DataFrames
-
-#untest
-#combine mutil part data
-"""
-    loadMultiData(filepath, filename, sort)
-
-documentation
-"""
-function loadMultiData(filepath, filename, sort = true)
-    #list all file in current folder
-    filelist = readdir(filepath, sort = sort)
-    data = DataFrame()
-    for i in filelist
-        data = [data; CSV.read(string(filepath, i))]
-        println(string("load data : ", i, " success."))
-    end
-    println("load data finished.")
-    return data
-end # function
 
 #Finished
 #search string in array
@@ -44,14 +25,52 @@ function searchArray(array, pattern)
     return tmp
 end # function
 
+#finished
+#combine mutil part data
+"""
+    loadMultiData(filepath, filename, sort)
+
+    Example:
+        data = loadMultiData("..\\data\\", "calendar")
+documentation
+"""
+function loadMultiData(filepath, filepattern, sort = true)
+    #list all file in current folder
+    filelist = searchArray(readdir(filepath, sort = sort), filepattern)
+    data = DataFrame()
+    for i in filelist
+        data = [data; CSV.read(string(filepath, i))]
+        println(string("load data : ", i, " success."))
+    end
+    println("load data finished.")
+    return data
+end # function
+
+
+
 #read data
 calendar=CSV.read("../data/calendar.csv")
 sales_train_validation=CSV.read("../data/sales_train_validation.csv")
 sample_submission=CSV.read("../data/sample_submission.csv")
 sell_prices=CSV.read("../data/sell_prices.csv")
 
-#Todo: data preproceesing
+#known data
+"""
+goal: Estimate the unit sales of Walmart retail goods.
+calendar.csv - Contains information about the dates on which the products are sold.
+sales_train_validation.csv - Contains the historical daily unit sales data per product and store [d_1 - d_1913]
+sample_submission.csv - The correct format for submissions. Reference the Evaluation tab for more info.
+sell_prices.csv - Contains information about the price of the products sold per store and date.
+sales_train_evaluation.csv - Available once month before competition deadline. Will include sales [d_1 - d_1941]
+"""
+
+println("rows length: ", size(df, 1), ", columns length: ", size(df, 2))
+
 #Todo: EDA
+using Plots
+x = 1:10; y = rand(10); # These are the plotting data
+plot(x, y)
+#Todo: data preproceesing
 #Todo: machine learning
 #Todo: predict
 #Todo: cross validation
